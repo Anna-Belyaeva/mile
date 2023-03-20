db37a0d4c9d11d930853a9d2aa3c42a772c7e14e
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-03-20 18:42:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        int milePrice = 20;
+
+        int mile = price / milePrice;
+
+        System.out.println(mile);
+
+    }
+}
